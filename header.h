e7d1937acd0b927152bf91d612688ff3e5d9530d
tutur90 <arthurgaron@gmail.com>
2023-12-18 08:38:56 +0100

    Question 3

--- header.h
+++ header.h
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <sys/socket.h>
 
 
 int parse_arguments(int argc, char *argv[], char **host, char **filename, char** localfile, char **port);
 void print_address(struct addrinfo *addr);
+int create_socket(struct addrinfo *result);
--- puttftp.c
+++ puttftp.c
@@ -20,17 +20,17 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    struct addrinfo hints, *result;
-    memset(&hints, 0, sizeof(struct addrinfo));
-
-    hints.ai_family = AF_UNSPEC;    // IPv4 or IPv6
-    hints.ai_socktype = SOCK_STREAM; // Stream socket
-
-    int status = getaddrinfo(host, port, &hints, &result);
-    if (status != 0) {
-        perror("getaddrinfo");
-        exit(EXIT_FAILURE);
+        // Print the resolved addresses
+    for (struct addrinfo *addr = result; addr != NULL; addr = addr->ai_next) {
+        print_address(addr);
     }
+
+    int sockfd = create_socket(result);
+
+    // Free memory allocated for address info
+    freeaddrinfo(result);
+    // Close Socket
+    close(sockfd);
 
     return EXIT_SUCCESS;
 }
